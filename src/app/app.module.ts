41f355b3cf36ebcd18c68800866141f0cf6434ec
Aanchal Shah <71883236+aanchalsh@users.noreply.github.com>
2023-08-29 19:10:18 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { WriteBlogComponent } from './write-blog/write-blog.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BlogListComponent } from './blog-list/blog-list.component';
 import { LoginComponent } from './login/login.component';
